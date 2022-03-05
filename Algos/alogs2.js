0a6eda3224a13137b02898b3f1d529cdf0253575
BenjyFrenkel <benyafrenya@gmail.com>
2022-03-04 16:21:32 -0800

    new commit

--- Algos/alogs2.js
+++ Algos/alogs2.js
@@ -8,8 +8,7 @@ function flatten(arr2d) {
         }
     }
     return flat;
-}
-    
+}    
 var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
 console.log(result); // we expect to get back [2, 5, 8, 3, 6, 1, 5, 7, 7]
 
